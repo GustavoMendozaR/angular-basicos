2b9ca59ab8517225110d738bb42d134e21d63eaa
Gustavo Mendoza <gusoda1992@gmail.com>
2022-10-15 23:24:05 -0500

    Primer Commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman','Hulk','Thor'];
+  heroeBorrado: string = ''
+  
+  borrarHeroe():void {
+    const heroeBorrado = this.heroes.shift();
+    this.heroeBorrado=heroeBorrado || ''
+    // console.log('Heroe Borrado:', heroeBorrado);
+  } 
+
+  // get heroeBorrado(): string{
+  //   return this.heroeBorrado
+  // }
+
+}
